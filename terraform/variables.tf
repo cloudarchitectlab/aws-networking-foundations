27c6dd2eedf334d34604fc1c264fbbd5ff2eaa5c
Rick P. <rickpond@protonmail.com>
2026-07-08 18:29:01 -0700

    Initialize Terraform foundation for AWS Networking case study

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region for this case study"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the case study VPC"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name used for tagging resources"
+  type        = string
+}
